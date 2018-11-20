577c1aabb409fdf0e7ba7cd8f6512c8162640084
starscreamnik <starscreamnik@gmail.com>
2018-11-20 13:04:27 +0200

    upgrade

--- Parallel multiplication/Parallel multiplication/kernel.cu
+++ Parallel multiplication/Parallel multiplication/kernel.cu
@@ -3,15 +3,12 @@
 #include <string>
 #include <algorithm>
 #include <cuda.h>
+#include <vector>
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
 using namespace std;
-
-const int N = 33 * 1024;
-const int threadsPerBlock = 256;
-const int blockPerGrid = min(32, (N + threadsPerBlock - 1) / threadsPerBlock);
 
 void errorHandler(cudaError_t error, const string& msg) {
 	if (error != cudaSuccess) {
@@ -20,83 +17,85 @@ void errorHandler(cudaError_t error, const string& msg) {
 	}
 }
 
-void input(ifstream& in, const int dim, int*& m1, int*& m2, int*& mAns, int*& v, int*& vAns) {
-	for (int i = 0; i < dim*dim; i++) in >> m1[i];
-	for (int i = 0; i < dim*dim; i++) in >> m2[i];
-	for (int i = 0; i < dim; i++) in >> v[i];
-	memset(vAns, 0, dim * sizeof(int));
-	memset(mAns, 0, dim*dim * sizeof(int));
+void input(ifstream& in, const int n, vector<int>& m1, vector<int>& m2, vector<int>& v) {
+	for (int i = 0; i < n*n; i++) in >> m1[i];
+	for (int i = 0; i < n*n; i++) in >> m2[i];
+	for (int i = 0; i < n; i++) in >> v[i];
 }
 
-void outputMxM(const int dim, int *a, int *b, int *ans) {
-	cout << "============Matrix on Matrix=============" << endl;
-	cout << "Parallel:\n";
-	for (int i = 0; i < dim; i++) {
-		for (int j = 0; j < dim; j++) {
-			cout << ans[i*dim + j] << " ";
+void outputMxM(ofstream& out, const int n, vector<int> a, vector<int> b, vector<int> ans) {
+	out << "============Matrix on Matrix=============" << endl;
+	out << "Parallel:\n";
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			out << ans[i*n + j] << " ";
 		}
-		cout << endl;
+		out << endl;
 	}
 
-	cout << "Consistent:\n";
-	for (int i = 0; i < dim; i++) {
-		for (int j = 0; j < dim; j++) {
+	out << "Consistent:\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
 			int c = 0;
-			for (int k = 0; k < dim; k++) {
-				c += a[dim*i + k] * b[dim*k + j];
+			for (int k = 0; k < n; k++) {
+				c += a[n*i + k] * b[n*k + j];
 			}
-			cout << c << " ";
+			out << c << " ";
 		}
-		cout << "\n";
+		out << "\n";
 	}
-	cout << endl;
+	out << endl;
 }
 
-void outputMxV(const int dim, int *m, int *v, int *ans) {
-	cout << "============Matrix on Vector=============" << endl;
-	cout << "Parallel:\n";
-	for (int i = 0; i < dim; i++) {
-		cout << ans[i] << " ";
+void outputMxV(ofstream& out, const int n, vector<int> m, vector<int> v, vector<int> ans) {
+	out << "============Matrix on Vector=============" << endl;
+	out << "Parallel:\n";
+	for (int i = 0; i < n; i++) {
+		out << ans[i] << " ";
 	}
-	cout << endl;
+	out << endl;
 
-	cout << "Consistent:\n";
-	for (int i = 0; i < dim; i++) {
+	out << "Consistent:\n";
+	for (int i = 0; i < n; i++) {
 		int c = 0;
-		for (int j = 0; j < dim; j++) {
-			c += m[i*dim + j] * v[j];
+		for (int j = 0; j < n; j++) {
+			c += m[i*n + j] * v[j];
 		}
-		cout << c << " ";
+		out << c << " ";
 	}
-	cout << endl;
+	out << endl;
 }
 
-__device__ int findRow(int tid, const int n) {
-	int counter = 0;
-	while (tid >= n) {
-		tid -= n;
-		counter++;
-	}
-	return counter;
-}
-
-__global__ void MxMKernel(const int *m1, const int *m2, int *c, int n) {
+__global__ void MxMKernel(const int *m1, const int *m2, int *ans, int n) {
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
 	int i = tid, j = tid;
 	if (tid < n*n) {
 		if (tid >= n) {
 			j %= n;
-			i = findRow(tid, n);
+			i /= n;
 		}
 		else i = 0;
-
 		for (int k = 0; k < n; k++)
-			c[tid] += m1[i*n + k] * m2[k*n + j];
+			ans[tid] += m1[i*n + k] * m2[k*n + j];
 	}
 }
 
+__global__ void MxMKernelS(const int *m1, const int *m2, int *ans, int n) {
+	int xInd = blockIdx.x * blockDim.x + threadIdx.x;
+	int yInd = blockIdx.y * blockDim.y + threadIdx.y;
+
+}
+
 __global__ void MxVKernel(const int *m, const int *v, int *ans, int n) {
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	if (tid < n) {
+		for (int k = 0; k < n; k++)
+			ans[tid] += m[k*n + tid] * v[k];
+	}
+}
+__global__ void MxVKernelnon(const int *m, const int *v, int *ans, int n) {
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
 	if (tid < n) {
 		for (int k = 0; k < n; k++)
@@ -106,39 +105,94 @@ __global__ void MxVKernel(const int *m, const int *v, int *ans, int n) {
 
 int main() {
 	ifstream in("input.txt");
-	int dim;
-	in >> dim;
-	const size_t dSize = sizeof(int)*dim*dim;
-	int *m1 = (int*)malloc(dSize), *m2 = (int*)malloc(dSize), *mAns = (int*)malloc(dSize);
-	int *v = (int*)malloc(sizeof(int)*dim), *vAns = (int*)malloc(sizeof(int)*dim);
+	ofstream out("output.txt");
+	cudaEvent_t start, stop;
+	errorHandler(cudaEventCreate(&start), "");
+	errorHandler(cudaEventCreate(&stop), "");
+	float time;
 
-	input(in, dim, m1, m2, mAns, v, vAns);
+	int n;
+	in >> n;
+	const size_t d1Size = sizeof(int)*n;
+	const size_t d2Size = d1Size*n;
+	vector<int> m1(n*n, 0), m2(n*n, 0), mAns(n*n, 0),
+				v(n, 0), vAns(n, 0);
+
+	input(in, n, m1, m2, v);
+
+	vector<int> trM1(n*n, 0);
+	for (int i = 0; i < n; i++) 
+		for (int j = 0; j < n; j++) 
+			trM1[i*n + j] = m1[j*n + i];
 
 	int *cuM1 = nullptr, *cuM2 = nullptr, *cuMAns = nullptr, *cuV = nullptr, *cuVAns = nullptr;
 
-	errorHandler(cudaMalloc(&cuM1, dSize), "cudaMalloc: cuM1");
-	errorHandler(cudaMalloc(&cuM2, dSize), "cudaMalloc: cuM2");
-	errorHandler(cudaMalloc(&cuMAns, dSize), "cudaMalloc: cuMAns");
-	errorHandler(cudaMalloc(&cuV, sizeof(int)*dim), "cudaMalloc: cuV");
-	errorHandler(cudaMalloc(&cuVAns, sizeof(int)*dim), "cudaMalloc: cuVAns");
+	int *cuTrM1 = nullptr;
+	errorHandler(cudaMalloc(&cuTrM1, d2Size), "cudaMalloc: cuTrM1");
+	errorHandler(cudaMemcpy(cuTrM1, trM1.data(), d2Size, cudaMemcpyHostToDevice), "cudaMemcpy: toDevice: cuTrM1");
 
-	errorHandler(cudaMemcpy(cuM1, m1, dSize, cudaMemcpyHostToDevice), "cudaMemcpy: toDevice: cuM1");
-	errorHandler(cudaMemcpy(cuM2, m2, dSize, cudaMemcpyHostToDevice), "cudaMemCpy: toDevice: cuM2");
-	errorHandler(cudaMemcpy(cuMAns, mAns, dSize, cudaMemcpyHostToDevice), "cudaMemCpy: toDevice: cuMAns");
-	errorHandler(cudaMemcpy(cuV, v, sizeof(int)*dim, cudaMemcpyHostToDevice), "cudaMemcpy: toDevice: cuV");
-	errorHandler(cudaMemcpy(cuVAns, vAns, sizeof(int)*dim, cudaMemcpyHostToDevice), "cudaMemcpy: toDevice: cuVAns");
+	errorHandler(cudaMalloc(&cuM1, d2Size), "cudaMalloc: cuM1");
+	errorHandler(cudaMalloc(&cuM2, d2Size), "cudaMalloc: cuM2");
+	errorHandler(cudaMalloc(&cuMAns, d2Size), "cudaMalloc: cuMAns");
+	errorHandler(cudaMalloc(&cuV, d1Size), "cudaMalloc: cuV");
+	errorHandler(cudaMalloc(&cuVAns, d1Size), "cudaMalloc: cuVAns");
 
-	MxMKernel << <blockPerGrid, threadsPerBlock >> > (cuM1, cuM2, cuMAns, dim);
+	errorHandler(cudaMemcpy(cuM1, m1.data(), d2Size, cudaMemcpyHostToDevice), "cudaMemcpy: toDevice: cuM1");
+	errorHandler(cudaMemcpy(cuM2, m2.data(), d2Size, cudaMemcpyHostToDevice), "cudaMemCpy: toDevice: cuM2");
+	errorHandler(cudaMemcpy(cuMAns, mAns.data(), d2Size, cudaMemcpyHostToDevice), "cudaMemCpy: toDevice: cuMAns");
+	errorHandler(cudaMemcpy(cuV, v.data(), d1Size, cudaMemcpyHostToDevice), "cudaMemcpy: toDevice: cuV");
+	errorHandler(cudaMemcpy(cuVAns, vAns.data(), d1Size, cudaMemcpyHostToDevice), "cudaMemcpy: toDevice: cuVAns");
+
+	const int threadsPerBlock = 256;
+	const int blockPerGrid = (n + threadsPerBlock - 1) / threadsPerBlock;
+	dim3 blockSize = dim3(threadsPerBlock, threadsPerBlock, 1);
+	dim3 gridSize = dim3(blockPerGrid, blockPerGrid, 1);
+
+	errorHandler(cudaEventRecord(start), "");
+	MxVKernelnon << <blockPerGrid, threadsPerBlock >> > (cuM1, cuV, cuVAns, n);
 	errorHandler(cudaGetLastError(), "cudaGetLastError");
-	errorHandler(cudaMemcpy(mAns, cuMAns, dSize, cudaMemcpyDeviceToHost), "cudaMemcpy: toHost: mAns");
-	outputMxM(dim, m1, m2, mAns);
+	errorHandler(cudaDeviceSynchronize(), "sync");
+	errorHandler(cudaMemcpy(vAns.data(), cuVAns, d1Size, cudaMemcpyDeviceToHost), "cudaMemcpy: toHost: vAns");
+	outputMxV(out, n, m1, v, vAns);
+	errorHandler(cudaEventRecord(stop), "" );
+	errorHandler(cudaEventSynchronize(stop), "");
+	errorHandler(cudaEventElapsedTime(&time, start, stop), "");
+	printf("MxV non-coalesced time X %f\n", time);
 
-	MxVKernel << <blockPerGrid, threadsPerBlock >> > (cuM1, cuV, cuVAns, dim);
+	vector<int> Empty(n, 0);
+	errorHandler(cudaMemcpy(cuVAns, Empty.data(), d1Size, cudaMemcpyHostToDevice), "cudaMemcpy: toDevice: cuVAns");
+
+	errorHandler(cudaEventRecord(start), "");
+	MxVKernel << <blockPerGrid, threadsPerBlock >> > (cuTrM1, cuV, cuVAns, n);
 	errorHandler(cudaGetLastError(), "cudaGetLastError");
-	errorHandler(cudaMemcpy(vAns, cuVAns, sizeof(int)*dim, cudaMemcpyDeviceToHost), "cudaMemcpy: toHost: vAns");
-	outputMxV(dim, m1, v, vAns);
+	errorHandler(cudaDeviceSynchronize(), "sync");
+	errorHandler(cudaMemcpy(vAns.data(), cuVAns, d1Size, cudaMemcpyDeviceToHost), "cudaMemcpy: toHost: vAns");
+	outputMxV(out, n, m1, v, vAns);
+	errorHandler(cudaEventRecord(stop), "");
+	errorHandler(cudaEventSynchronize(stop), "");
+	errorHandler(cudaEventElapsedTime(&time, start, stop), "");
+	printf("MxV coalesced time X %f\n", time);
 
-	free(m1), free(m2), free(mAns), free(v), free(vAns);
+	//errorHandler(cudaEventRecord(start), "");
+	//MxMKernel << <gridSize, blockSize >> > (cuM1, cuM2, cuMAns, n);
+	//
+	//errorHandler(cudaGetLastError(), "cudaGetLastError");
+	//errorHandler(cudaDeviceSynchronize(), "sync");
+	//errorHandler(cudaMemcpy(vAns.data(), cuVAns, d1Size, cudaMemcpyDeviceToHost), "cudaMemcpy: toHost: vAns");
+	//outputMxM(out, n, m1, v, vAns);
+	//errorHandler(cudaEventRecord(stop), "");
+	//errorHandler(cudaEventSynchronize(stop), "");
+	//errorHandler(cudaEventElapsedTime(&time, start, stop), "");
+	//printf("coalesced time X %f\n", time);
+
+
+	//MxMKernel << <blockPerGrid, threadsPerBlock >> > (cuM1, cuM2, cuMAns, n);
+	//errorHandler(cudaGetLastError(), "cudaGetLastError");
+	//errorHandler(cudaDeviceSynchronize(), "sync");
+	//errorHandler(cudaMemcpy(mAns, cuMAns, d2Size, cudaMemcpyDeviceToHost), "cudaMemcpy: toHost: mAns");
+	//outputMxM(out, n, m1, m2, mAns);
+
+	errorHandler(cudaFree(cuTrM1), "cudaFree: cuTrM1");
 	errorHandler(cudaFree(cuM1), "cudaFree: cuM1");
 	errorHandler(cudaFree(cuM2), "cudaFree: cuM2");
 	errorHandler(cudaFree(cuMAns), "cudaFree: cuMAns");
