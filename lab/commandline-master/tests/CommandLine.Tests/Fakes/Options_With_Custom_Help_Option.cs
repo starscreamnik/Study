809a91488ce4af551ab60bf6848a7f23c897dcd7
Nik <31962595+starscreamnik@users.noreply.github.com>
2019-01-26 13:31:18 +0200

    remove

--- lab/commandline-master/tests/CommandLine.Tests/Fakes/Options_With_Custom_Help_Option.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CommandLine.Tests.Fakes
-{
-    class Options_With_Custom_Help_Option : Simple_Options
-    {
-        [Option('h', "help")]
-        public bool Help { get; set; }
-    }
-}
--- lab/commandline-master/tests/CommandLine.Tests/Fakes/Options_With_Custom_Version_Option.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CommandLine.Tests.Fakes
-{
-    class Options_With_Custom_Version_Option : Simple_Options
-    {
-        [Option('v', "version")]
-        public bool MyVersion { get; set; }
-    }
-}
