3a05f8410c15278477be3037cc5ba9b61387858c
Nik <31962595+starscreamnik@users.noreply.github.com>
2019-04-28 16:11:48 +0200

    New C# project. Low memory storage of bits through a List<byte>. Realization of IEnumerable, indexator.

---
+++ BitContainer/BitContainer/BitContainer.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+
+namespace BitContainer
+{
+    public class BitContainer
+    {
+        private List<byte> data = new List<byte>();
+
+        public int Length = 0;
+        
+        public bool this [int index] {
+            get
+            {
+                var i = index / 8;
+                var shift = index % 8;
+                return (data[i] & (1 << shift )) != 0;
+            }
+
+            set
+            {
+                var i = index / 8;
+                var shift = index % 8;
+                if (value)
+                    data[i] |= (byte) (1 << shift);
+                 else
+                    data[i] &= (byte) ~(1 << shift);
+            }
+        }
+
+        public void PushBit(int bit)
+        {
+            if (Length % 8 == 0) data.Add(0);    // push fictive value to list
+            this[Length] = bit == 1;
+            Length++;
+        }
+
+        public void PushBit(bool bit)
+        {
+            if (Length % 8 == 0) data.Add(0);    // push fictive value to list
+            this[Length] = bit;
+            Length++;
+        }
+
+        public void Clear()
+        {
+            data.Clear();
+            Length = 0;
+        }
+
+        public void Insert(int place, int bit)
+        {
+            try
+            {
+                if (place < 0 || place >= Length)
+                    throw new IndexOutOfRangeException();
+                
+                if (Length % 8 == 0) data.Add(0);
+
+                for (var i = Length; i > place; i--)
+                {
+                    this[i] = this[i - 1];
+                }
+
+                this[place] = bit == 1;
+
+                Length++;
+            }
+            catch (IndexOutOfRangeException msg)
+            {
+                Console.WriteLine("INSERT: {0}", msg);
+            }
+        }
+        
+        public void Insert(int place, bool bit)
+        {
+            try
+            {
+                if (place < 0 || place >= Length)
+                    throw new IndexOutOfRangeException();
+                
+                if (Length % 8 == 0) data.Add(0);
+
+                for (var i = Length; i > place; i--)
+                {
+                    this[i] = this[i - 1];
+                }
+
+                this[place] = bit;
+
+                Length++;
+            }
+            catch (IndexOutOfRangeException msg)
+            {
+                Console.WriteLine("INSERT: {0}", msg);
+            }
+        }
+
+        public void Remove(int place)
+        {
+            try
+            {
+                if (place < 0 || place >= Length)
+                    throw new IndexOutOfRangeException();
+                
+                for (var i = place; i < Length - 1; i++)
+                {
+                    this[i] = this[i + 1];
+                }
+
+                Length--;
+
+                if (Length % 8 == 0)
+                {
+                    data.RemoveAt(data.Count - 1);
+                }
+            }
+            catch (IndexOutOfRangeException msg)
+            {
+                Console.WriteLine("REMOVE: {0}", msg);
+            }
+
+        }
+
+        public IEnumerable<bool> MyEnumerator()
+        {
+            for (var i = 0; i < Length; i++)
+                yield return this[i];
+        }
+
+        public string ToString(bool bit)
+        {
+            return bit? "1" : "0";
+        }
+        
+    }
+}
---
+++ BitContainer/BitContainer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace BitContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new BitContainer();
+            
+            for (var i = 0; i < 8; i++)
+                b.PushBit(1);
+            Console.WriteLine("ADD 8 bits, Bytes: {0}", b);
+            for (var i = 0; i<b.Length; i++)
+                Console.Write("{0} ", b.ToString(b[i]));
+            Console.WriteLine();
+            
+            
+            for (var i = 0; i < 7; i++)
+                b.PushBit(false);
+            Console.WriteLine("ADD 7 bits");
+            for (var i = 0; i<b.Length; i++)
+                Console.Write("{0} ", b.ToString(b[i]));
+            Console.WriteLine();
+
+            
+            b.Insert(5, 1);
+            Console.WriteLine("INSERT true TO 5");
+            foreach (var bit in b.MyEnumerator()) 
+                Console.Write("{0} ", b.ToString(bit));
+            Console.WriteLine();
+            
+            
+            b.Insert(1, false);
+            Console.WriteLine("INSERT false TO 1");
+            foreach (var bit in b.MyEnumerator())
+                Console.Write("{0} ", b.ToString(bit));
+            Console.WriteLine();
+            
+           
+            b.Remove(0);
+            Console.WriteLine("REMOVE FROM 0");
+            foreach (var bit in b.MyEnumerator())
+                Console.Write("{0} ", b.ToString(bit));
+            Console.WriteLine();
+
+//            b.Insert(-1, false);
+//            b.Insert(b.Length, true);
+//            b.Remove(20);
+            
+            b.Clear();
+            Console.WriteLine("CLEAR");
+            foreach (var bit in b.MyEnumerator())
+                Console.Write("{0} ", b.ToString(bit));
+            Console.WriteLine();
+        }
+        
+    }
+}
