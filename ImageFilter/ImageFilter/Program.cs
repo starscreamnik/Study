fa9608b703ec9911a3a2961d9cf2a0fec286012c
Nik <31962595+starscreamnik@users.noreply.github.com>
2019-05-05 12:48:25 +0200

    Console input of the path to image

--- ImageFilter/ImageFilter/Program.cs
+++ ImageFilter/ImageFilter/Program.cs
@@ -8,21 +8,23 @@ namespace ImageFilter
     {    
         static void Main(string[] args)
         {
-            var filter1 = new MatrixFilter(11);
-            var image = new Bitmap(@"C:/Users/Nikita/Desktop/img.jpg");
+            var filter1 = new MatrixFilter(3);
+            Console.Write("Введите путь к изображению: ");
+            var path = Console.ReadLine();
+            var image = new Bitmap(path);
 
             DateTime start = DateTime.Now;
             var res1 = filter1.RunGaussianBlur(image, false);
             TimeSpan fin = DateTime.Now - start;
             Console.WriteLine("normal mode: time = {0}", fin.ToString());
-            res1.Save(@"C:/Users/Nikita/Desktop/normal.jpg");
+            res1.Save("normal.jpg");
 
             
             start = DateTime.Now;
             var res2 = filter1.RunGaussianBlur(image, true);
             fin = DateTime.Now - start;
             Console.WriteLine("unsafe mode: time = {0}", fin.ToString());
-            res2.Save(@"C:/Users/Nikita/Desktop/.jpg");
+            res2.Save("unsafe.jpg");
         }
     }
 }
